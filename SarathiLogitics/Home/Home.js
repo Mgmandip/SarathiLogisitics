05260e53e1f8f3e6b3d8364de5a30aa0613993ce
Mgmandip <mandeepmgr74@gmail.com>
2025-08-20 17:27:23 +0545

    Services Updated

--- SarathiLogitics/Home/Home.js
+++ SarathiLogitics/Home/Home.js
@@ -1,17 +1,21 @@
 function switchTab(tab) {
-  const tabs = document.querySelectorAll('.tab-content');
-  const buttons = document.querySelectorAll('.tab-btn');
+  const tabs = document.querySelectorAll(".tab-content");
+  const buttons = document.querySelectorAll(".tab-btn");
 
-  tabs.forEach(t => t.classList.remove('active'));
-  buttons.forEach(b => b.classList.remove('active'));
+  tabs.forEach((t) => t.classList.remove("active"));
+  buttons.forEach((b) => b.classList.remove("active"));
 
-  document.getElementById(tab + '-tab').classList.add('active');
-  document.querySelector(`.tab-btn[onclick="switchTab('${tab}')"]`).classList.add('active');
+  document.getElementById(tab + "-tab").classList.add("active");
+  document
+    .querySelector(`.tab-btn[onclick="switchTab('${tab}')"]`)
+    .classList.add("active");
 }
 
+const learnMoreButtons = document.querySelectorAll(".learnMoreBtn");
 
- const learnMoreBtn = document.getElementById('learnMoreBtn');
-
-    learnMoreBtn.addEventListener('click', function() {
-        window.location.href = '../S';
-    });
+learnMoreButtons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const targetPage = this.dataset.target;
+    window.location.href = targetPage;
+  });
+});

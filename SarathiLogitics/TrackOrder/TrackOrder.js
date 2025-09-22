331b86189d79fa6c61fa2e612ff5454f5ad8798f
Mgmandip <mandeepmgr74@gmail.com>
2025-09-22 17:42:13 +0545

    Sarathi Updated

--- SarathiLogitics/TrackOrder/TrackOrder.js
+++ SarathiLogitics/TrackOrder/TrackOrder.js
@@ -187,10 +187,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-/* Menu */
-const menuToggle = document.getElementById("menu-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-menuToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
